35f5eb902faf8f50b5772da9eac92ce51b467cd5
heshamamoudi <hesham69mez@gmail.com>
2022-04-21 18:37:02 +0300

    fixed end point testing

--- src/handlers/tests/EndpointsSpec.ts
+++ src/handlers/tests/EndpointsSpec.ts
@@ -1,18 +1,15 @@
 import * as supertest from 'supertest';
-import * as express from 'express'
+import app from '../../server'
 
-
-const app: express.Application = express();
 const request = supertest(app);
 
 
 
 describe('Test responses from endpoints', (): void => {
     let token:string ;
-    
+
     it('GET all users', async function ():Promise<void> {
         const response: supertest.Response = await request.get('/users');
-        console.log(response.body)
         expect(response.status).toBe(200);
       });
 
@@ -23,11 +20,24 @@ describe('Test responses from endpoints', (): void => {
           username:"heshamamoudi",
           password: 'helloworld123',
         });
-        
-        console.log(response.body)
+        token=response.body.token;
         expect(response.status).toBe(200);
         
       });
+      
+      it('Create user', async function ():Promise<void> {
+        const response = await request.post('/signup').send({
+          firstname: 'raman',
+          lastname: 'raghav',
+          username:"heshamamoudi",
+          password: 'helloworld123',
+        });
+        token=response.body.token;
+        expect(response.status).toBe(200);
+        
+      });
+
+
       
     });
 
